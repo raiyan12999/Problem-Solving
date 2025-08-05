afd0d16fa0527e3a107444e0eef15431808983fe
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-05 10:45:29 -0700

    Started lesson for Array and Arraylist

--- ValidAnagram/VAnagram.java
+++ ValidAnagram/VAnagram.java
@@ -1,6 +1,12 @@
-package ValidAnagram;
 public class VAnagram {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+
+        for (String s : strs){
+            char[] chars = s.toCharArray();
+        }
+
+        System.out.println("Hello!");
+    
     }
 }
