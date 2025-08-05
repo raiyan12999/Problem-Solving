6d1a557feb77b059cb368c5b92c57c373f5c14bc
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-05 10:53:07 -0700

    created first array

--- Array/Lesson1.java
+++ Array/Lesson1.java
@@ -1,7 +1,17 @@
-package Array;
-
 public class Lesson1 {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        
+        //Syntax 
+        // datatype[] variable_name = new datatype[size];
+
+
+        int[] rollNo = new int[5];
+
+
+        //or directly
+        int[] rollNo2 = {34, 4, 2, 5, 7};
+
+        System.out.println("first roll No " + rollNo2[0]);
+
     }
 }
