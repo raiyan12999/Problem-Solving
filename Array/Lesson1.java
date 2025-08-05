6b512ed47ec8b276c76859fdcf3ae37bc481e255
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-05 10:59:16 -0700

    Internals of array

--- Array/Lesson1.java
+++ Array/Lesson1.java
@@ -5,13 +5,17 @@ public class Lesson1 {
         // datatype[] variable_name = new datatype[size];
 
 
-        int[] rollNo = new int[5];
+        int[] rollNo = new int[5]; // rollNo reference variable is pointing to an array object that contains integer type
+
 
 
         //or directly
         int[] rollNo2 = {34, 4, 2, 5, 7};
 
         System.out.println("first roll No " + rollNo2[0]);
+
+        int[] rollNo3; // Declaration: Only the reference variable is created in the stack
+        rollNo3 = new int[6]; // Initialization: Memory creation. The array object is created in the heap. 
 
     }
 }
